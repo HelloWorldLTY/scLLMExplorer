525ac6a401eeca29371e9defb7d3ccc3f483c837
caozhiyuan0424 <67963113+caozhiyuan0424@users.noreply.github.com>
2023-12-18 15:12:18 -0500

    Update deconv_main.py

---
+++ deconv_main.py
@@ -0,0 +1,46 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
+from transformers import TextDataset, DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+import pytorch_lightning
+pytorch_lightning.seed_everything(0) # set seed.
+model_name,train_file, test_file, output_dir = "gpt2", "pancreas_train_input_deconv.txt", "pancreas_test_input_deconv.txt", "gpt2_annot2"
+
+# Load GPT-2 model and tokenizer
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+# Load training dataset
+train_dataset = TextDataset(
+    tokenizer=tokenizer,
+    file_path=train_file,
+    block_size=1024)
+test_dataset = TextDataset(
+    tokenizer=tokenizer,
+    file_path=test_file,
+    block_size=1024)
+
+# Create data collator for language modeling
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=False)
+# Set training arguments
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    overwrite_output_dir=True,
+    num_train_epochs=1,
+    per_device_train_batch_size=4,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+# Train the model
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset
+)
+trainer.train()
+# Save the fine-tuned model
+model.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
